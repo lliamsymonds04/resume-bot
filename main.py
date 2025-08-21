45431e61f76015cc655e303c7bef54ec74083cb9
Lliam <lliamsymonds04@gmail.com>
2025-08-21 18:19:09 +1000

    added stealth mode

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import asyncio
-from crawl4ai import AsyncWebCrawler
+from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+jobsite = os.getenv("JOBSITE")
+if not jobsite:
+    raise ValueError("JOBSITE environment variable not set")
+
 
 async def main():
+    browser_config = BrowserConfig(
+        enable_stealth=True,
+        headless=False,
+    )
+
+    run_config = CrawlerRunConfig()
     # Create an instance of AsyncWebCrawler
-    async with AsyncWebCrawler() as crawler:
+    async with AsyncWebCrawler(config=browser_config) as crawler:
         # Run the crawler on a URL
-        result = await crawler.arun(url="https://www.seek.com.au/")
+        result = await crawler.arun(url=jobsite, run_config=run_config)
 
         # Print the extracted content
         print(result.markdown)
