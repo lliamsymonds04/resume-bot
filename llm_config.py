b557b59a1a4dd0fa1df37f03183cb54031c7c498
Lliam <lliamsymonds04@gmail.com>
2025-08-24 16:41:08 +1000

    added ai folder

--- llm_config.py
+++
@@ -1,8 +0,0 @@
-from langchain_groq import ChatGroq
-import os
-
-#get the model
-model_name = os.getenv("MODEL_NAME")
-api_key = os.getenv("GROQ_API_KEY")
-
-llm = ChatGroq(model_name=model_name, api_key=api_key, temperature=0)
