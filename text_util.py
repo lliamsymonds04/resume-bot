65a3a2cc7db33ed94e31b8650ed7f3ec4a81d076
Lliam <lliamsymonds04@gmail.com>
2025-08-24 16:05:23 +1000

    added text util

---
+++ text_util.py
@@ -0,0 +1,23 @@
+import os
+
+def get_text_folder() -> str:
+    # make it if it doesnt exist
+    os.makedirs("texts", exist_ok=True)
+    return "texts"
+
+def append_filename_to_folder(filename: str) -> str:
+    folder = get_text_folder()
+    return os.path.join(folder, filename)
+
+def save_text(text: str, filename: str) -> None:
+    """Save text to a file."""
+    filename = append_filename_to_folder(filename)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+
+
+def load_text(filename: str) -> str:
+    """Load text from a file."""
+    filename = append_filename_to_folder(filename)
+    with open(filename, "r", encoding="utf-8") as f:
+        return f.read()
