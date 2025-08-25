b0c5a1f133cdb5f75380e3ab47c07947da5e54af
Lliam <lliamsymonds04@gmail.com>
2025-08-25 16:34:23 +1000

    added project tailoring

---
+++ ai/tailor_projects.py
@@ -0,0 +1,38 @@
+import json
+from models.job_description import JobDescription
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+def tailor_projects(job_description: JobDescription, num_projects: int = 3):
+    #load the projects json
+    with open("data/projects.json", "r") as f:
+        projects_data = json.load(f)
+
+    # print(projects_data)
+
+    project_docs = []
+    for project in projects_data:
+        # Combine title and description for better matching
+        content = f"Title: {project['title']}\nDescription: {project['description']}"
+        # Store the full project data in metadata
+        doc = Document(page_content=content, metadata=project)
+        project_docs.append(doc)
+
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vs = FAISS.from_documents(project_docs, embeddings)
+
+    prompt = f"""
+    Based on the following job description, find the most relevant projects:
+    {job_description.model_dump()}
+
+    Here are the top {num_projects} projects:
+    """
+
+    retrieved_docs = vs.similarity_search(prompt, k=num_projects)
+
+    for doc in retrieved_docs:
+        print(f"Title: {doc.metadata['title']}")
+        print(f"Description: {doc.metadata['description']}")
+        print()
---
+++ tailor.py
@@ -0,0 +1,23 @@
+from ai.tailor_projects import tailor_projects
+from ai.parse_job_description import parse_job_description
+from ai.llm_config import get_llm
+
+import os
+from dotenv import load_dotenv
+from scraping.scrape_job_info import scrape_job_info
+from util.text_util import load_text, save_text
+
+load_dotenv()
+link = os.getenv("TEST_JOB_LINK")
+
+if os.path.exists("texts/job_info.txt"):
+    with open("texts/job_info.txt", "r") as f:
+        # job_info = f.read()
+        job_info = load_text("job_info")
+else:
+    job_info = scrape_job_info(link)
+    save_text(job_info, "job_info")
+
+llm = get_llm()
+job_d = parse_job_description(job_info, llm)
+tailor_projects(job_d)
