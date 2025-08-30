05e9774f0aee731b4c6fd7ef944a0ddb14ed0bde
Lliam <lliamsymonds04@gmail.com>
2025-08-30 11:02:14 +1000

    added ascii art to terminal

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.layout import Layout, HSplit, Window
 from prompt_toolkit.layout.controls import FormattedTextControl
 
+ascii_art = r"""__________                                     __________        __   
+\______   \ ____   ________ __  _____   ____   \______   \ _____/  |_ 
+ |       _// __ \ /  ___/  |  \/     \_/ __ \   |    |  _//  _ \   __\
+ |    |   \  ___/ \___ \|  |  /  Y Y  \  ___/   |    |   (  <_> )  |  
+ |____|_  /\___  >____  >____/|__|_|  /\___  >  |______  /\____/|__|  
+        \/     \/     \/            \/     \/          \/             """
+ascii_art += "\n" + "="*70 + "\n"
+
 def render():
     frags = []
-    frags.append(("", "Hello, World!\n"))
-    
+    frags.append(("", ascii_art))
     return frags
     
 control = FormattedTextControl(render, focusable=True)
