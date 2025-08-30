f4892999955eddb348170c7b513612ed7e458a2a
Lliam <lliamsymonds04@gmail.com>
2025-08-30 11:46:28 +1000

    added option selection to landing screen

--- screens/landing_screen.py
+++ screens/landing_screen.py
@@ -5,24 +5,38 @@ from prompt_toolkit.layout.controls import FormattedTextControl
 
 from screens.screen_base import Screen
 
+line_len = 70
+
 ascii_art = r"""__________                                     __________        __   
 \______   \ ____   ________ __  _____   ____   \______   \ _____/  |_ 
  |       _// __ \ /  ___/  |  \/     \_/ __ \   |    |  _//  _ \   __\
  |    |   \  ___/ \___ \|  |  /  Y Y  \  ___/   |    |   (  <_> )  |  
  |____|_  /\___  >____  >____/|__|_|  /\___  >  |______  /\____/|__|  
         \/     \/     \/            \/     \/          \/             """
-ascii_art += "\n" + "="*70 + "\n"
+ascii_art += "\n" + "="*line_len + "\n"
 
+options = ["Config", "Seek", "Something else idk"]
 
 class LandingScreen(Screen):
     def __init__(self):
         super().__init__("landing")
         self.control = FormattedTextControl(self.render, focusable=True)
         self.container = HSplit([Window(content=self.control, always_hide_cursor=True)])
+        self.state = {"selection": 0}
 
     def render(self):
         frags = []
         frags.append(("", ascii_art))
+        frags.append(("", "Welcome to Resume Bot!\n\n"))
+        frags.append(("", "Options:\n"))
+
+        # render options
+        for i, option in enumerate(options):
+            prefix = "👉 " if i == self.state["selection"] else "   "
+            style = "reverse" if i == self.state["selection"] else ""
+            frags.append((style, f"{prefix}{option}\n"))
+
+        frags.append(("", "\n" + "="*line_len+"\n"))
         frags.append(("", "\nPress Q to quit.\n"))
         return frags
 
@@ -35,5 +49,17 @@ class LandingScreen(Screen):
         @kb.add("q")
         def _(event):
             event.app.exit()
+
+        @kb.add("j")
+        def _(event):
+            self.state["selection"] = (self.state["selection"] + 1) % len(options)
+            self.control.text = self.render()
+            event.app.invalidate()
+
+        @kb.add("k")
+        def _(event):
+            self.state["selection"] = (self.state["selection"] - 1) % len(options)
+            self.control.text = self.render()
+            event.app.invalidate()
 
         return kb
