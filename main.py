50643aea5b6505c9c5176f2ad351713649e8530f
Lliam <lliamsymonds04@gmail.com>
2025-08-30 10:53:34 +1000

    added basic command window

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-import os
-from dotenv import load_dotenv
-from scraping.scrape_job_info import scrape_job_info
-from util.text_util import save_text
+from prompt_toolkit import Application
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.layout import Layout, HSplit, Window
+from prompt_toolkit.layout.controls import FormattedTextControl
 
-load_dotenv()
-link = os.getenv("TEST_JOB_LINK")
+def render():
+    frags = []
+    frags.append(("", "Hello, World!\n"))
+    
+    return frags
+    
+control = FormattedTextControl(render, focusable=True)
+container = HSplit([Window(content=control, always_hide_cursor=True)])
 
-job_info = scrape_job_info(link)
-save_text(job_info, "job_info.txt")
+
+kb = KeyBindings()
+
+@kb.add("q")
+def _(event):
+    event.app.exit()
+
+
+app = Application(layout=Layout(container), key_bindings=kb, full_screen=True)
+
+
+if __name__ == "__main__":
+    app.run()
