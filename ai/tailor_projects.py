f025b328ad5df44df3a0f028670c84229be96d3d
Lliam <lliamsymonds04@gmail.com>
2025-09-03 16:20:10 +1000

    reworked tailoring to only make 1 api call

--- ai/tailor_projects.py
+++ ai/tailor_projects.py
@@ -6,6 +6,9 @@ from langchain_core.documents import Document
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel
+class Projects(BaseModel):
+    projects: list[Project]
 
 async def tailor_projects(job_description: JobDescription, num_projects: int = 3):
     #load the projects json
@@ -35,11 +38,15 @@ async def tailor_projects(job_description: JobDescription, num_projects: int = 3
 
     retrieved_docs = vs.similarity_search(prompt, k=num_projects)
 
-    tailored_projects = []
-    for doc in retrieved_docs:
+    #! dont delete this code. Could reuse in a coroutine
+    # tailored_projects = []
+    # for doc in retrieved_docs:
         # Expand the project to be more relevant to the job description
-        p = await expand_project_for_job(job_description, Project(**doc.metadata))
-        tailored_projects.append(p)
+        # p = await expand_project_for_job(job_description, Project(**doc.metadata))
+        # tailored_projects.append(p)
+
+    base_projects = Projects(projects=[Project(**doc.metadata) for doc in retrieved_docs])
+    tailored_projects = await expand_projects_for_job(job_description, base_projects)
 
     return tailored_projects
 
@@ -69,4 +76,32 @@ async def expand_project_for_job(job_description: JobDescription, project: Proje
 
     response = await llm.ainvoke(prompt)
 
+    return parser.parse(response.content)
+
+async def expand_projects_for_job(job_description: JobDescription, projects: Projects):
+    llm = get_llm(0.3)
+
+    parser = PydanticOutputParser(pydantic_object=Projects)
+
+    prompt = f"""
+        Given the following job description:
+        {job_description.model_dump()}
+
+        And the projects are:
+        {projects.model_dump()}
+        you should return a list of expanded projects. There should be {len(projects.projects)}.
+
+        Expand on the projects to make them more relevant to the job description.
+        Do not change the title
+        Do not lie about my skills or process
+        Do not talk about programming languages or tools that i did not use.
+        Always mention the programming languages and the tools
+        Keep the description concise and focused on the key aspects. It is for a resume
+        Do not talk about external teams or stakeholders if none are mentioned
+
+        Format instructions:
+        {parser.get_format_instructions()}
+        """
+    
+    response = await llm.ainvoke(prompt)
     return parser.parse(response.content)
