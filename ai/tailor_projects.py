0ef16f4b9160ecd0d7f73b478659060f1fbda171
Lliam Symonds <lliamsymonds04@gmail.com>
2025-11-06 00:44:50 +1000

    removed old functions and dependencies

--- ai/tailor_projects.py
+++ ai/tailor_projects.py
@@ -2,9 +2,6 @@ import json
 from ai.llm_config import get_llm
 from models.job_description import JobDescription
 from models.project import Project
-from langchain_core.documents import Document
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel
 
@@ -54,62 +51,3 @@ async def tailor_projects(job_description: JobDescription, num_projects: int = 3
     )
 
     return projects_parser.parse(response.content)
-
-
-async def expand_project_for_job(job_description: JobDescription, project: Project):
-    llm = get_llm(0.3)
-
-    parser = PydanticOutputParser(pydantic_object=Project)
-    prompt = f"""
-    Given the following job description:
-    {job_description.model_dump()}
-
-    And the project details:
-    {project.model_dump()}
-
-    Expand on the project to make it more relevant to the job description.
-    Do not change the title
-    Do not lie about my skills or process
-    Do not talk about programming languages or tools that i did not use.
-    Always mention the programming languages and the tools
-    Keep the description concise and focused on the key aspects. It is for a resume
-    Do not talk about external teams or stakeholders if none are mentioned
-    Talk about the cloud technologies used if present in the project
-
-    Format instructions:
-    {parser.get_format_instructions()}
-    """
-
-    response = await llm.ainvoke(prompt)
-
-    return parser.parse(response.content)
-
-
-async def expand_projects_for_job(job_description: JobDescription, projects: Projects):
-    llm = get_llm(0.3)
-
-    parser = PydanticOutputParser(pydantic_object=Projects)
-
-    prompt = f"""
-        Given the following job description:
-        {job_description.model_dump()}
-
-        And the projects are:
-        {projects.model_dump()}
-        you should return a list of expanded projects. There should be {len(projects.projects)}.
-
-        Expand on the projects to make them more relevant to the job description.
-        Do not change the title
-        Do not lie about my skills or process
-        Do not talk about programming languages or tools that i did not use.
-        Always mention the programming languages and the tools
-        Keep the description concise and focused on the key aspects. It is for a resume
-        Do not talk about external teams or stakeholders if none are mentioned
-
-        Format instructions:
-        {parser.get_format_instructions()}
-        """
-
-    response = await llm.ainvoke(prompt)
-    return parser.parse(response.content)
-
